af869cb062e5c1fb5d54e70cd2c572759847984f
Kauane Farias <102301521+kaufariax@users.noreply.github.com>
2026-02-23 20:02:28 -0300

    Feature/entityframework (#8)
    * Estruturação do Projeto
    
    * Inserindo Entity Framework

--- Backend/CarZone/Program.cs
+++ Backend/CarZone/Program.cs
@@ -1,4 +1,13 @@
+using CarZone.src.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Configura a conex�o com o banco de dados
+builder.Services.AddDbContext<CarZoneContexto>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 // Add services to the container.
 
---
+++ Backend/CarZone/src/Models/Veiculo.cs
@@ -0,0 +1,12 @@
+﻿namespace CarZone.src.Models
+{
+    public class Veiculo : ModeloBase
+    {
+        
+        public string Modelo { get; set; }
+        public string Marca { get; set; }
+        public int Ano { get; set; }
+        public decimal Valor { get; set; }
+        public string Tipo { get; set; }
+    }
+}
