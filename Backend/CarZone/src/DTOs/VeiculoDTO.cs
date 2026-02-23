8d9a181be20cfde826596b5010dec8248a066546
kaufariax <eikauanefarias@gmail.com>
2026-02-23 19:59:32 -0300

    Implementação de DTOs

---
+++ Backend/CarZone/src/DTOs/VeiculoDTO.cs
@@ -0,0 +1,32 @@
+﻿namespace CarZone.src.DTOs
+{
+    public class NovoVeiculoDTO
+    {
+        public int ID { get; set; }
+        public string Modelo { get; set; }
+        public string Marca { get; set; }
+        public int Ano { get; set; }
+        public decimal Valor { get; set; }
+        public string Tipo { get; set; }
+        public DateTime CriadoEm { get; set; }
+    }
+
+    public class AtualizarVeiculoDTO
+    {
+        public int ID { get; set; }
+        public string Modelo { get; set; }
+        public string Marca { get; set; }
+        public int Ano { get; set; }
+        public decimal Valor { get; set; }
+        public string Tipo { get; set; }
+    }
+
+    public class ListarVeiculosDTO
+    {
+        public string Modelo { get; set; }
+        public string Marca { get; set; }
+        public int Ano { get; set; }
+        public decimal Valor { get; set; }
+        public string Tipo { get; set; }
+    }
+}
