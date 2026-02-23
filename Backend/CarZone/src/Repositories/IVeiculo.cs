b0b2e81a7a7e2fc4bb7ca72e25431b8ce0167820
Kauane Farias <102301521+kaufariax@users.noreply.github.com>
2026-02-23 20:16:27 -0300

    Implementação de DTOs (#10)

---
+++ Backend/CarZone/src/Repositories/IVeiculo.cs
@@ -0,0 +1,14 @@
+﻿using CarZone.src.DTOs;
+using CarZone.src.Models;
+
+namespace CarZone.src.Repositories
+{
+    public interface IVeiculo
+    {
+        Task NovoVeiculoAsync(NovoVeiculoDTO veiculo);
+        Task<List<ListarVeiculosDTO>> ListarTodosVeiculosAsync();
+        Task AtualizarVeiculoAsync(AtualizarVeiculoDTO veiculo);
+        Task DeletarVeiculoAsync(int id);
+
+    }
+}
