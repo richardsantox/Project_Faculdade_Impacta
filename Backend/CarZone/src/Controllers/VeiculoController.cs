3c4416f68a1f694e4518dc841f6156c91e1e3715
kaufariax <eikauanefarias@gmail.com>
2026-04-11 20:17:12 -0300

    Nova rota Criação de veiculos

--- Backend/CarZone/src/Controllers/VeiculoController.cs
+++ Backend/CarZone/src/Controllers/VeiculoController.cs
@@ -1,4 +1,5 @@
-﻿using CarZone.src.Repositories;
+﻿using CarZone.src.DTOs;
+using CarZone.src.Repositories;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CarZone.src.Controllers
@@ -7,7 +8,6 @@ namespace CarZone.src.Controllers
     [Route("api/Veiculos")]
     public class VeiculoController : ControllerBase
     {
-
         private readonly IVeiculo _repositorio;
 
         public VeiculoController(IVeiculo repositorio)
@@ -23,6 +23,31 @@ namespace CarZone.src.Controllers
             if (lista.Count == 0) return NoContent();
 
             return Ok(lista);
+        }
+
+        [HttpPost("upload")]
+        public async Task<ActionResult> NovoVeiculoAsync(
+            [FromForm] NovoVeiculoDTO veiculo, IFormFile imagem)
+        {
+            string fileName = string.Empty;
+            if (imagem != null && imagem.Length > 0)
+            {
+                var caminhoPasta = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "imagens");
+                Directory.CreateDirectory(caminhoPasta);
+
+                var nomeArquivo = $"{Guid.NewGuid().ToString()}_{imagem.FileName}";
+                var caminhoArquivo = Path.Combine(caminhoPasta, nomeArquivo);
+                fileName = nomeArquivo;
+
+                using (var stream = new FileStream(caminhoArquivo, FileMode.Create))
+                {
+                    await imagem.CopyToAsync(stream);
+                }
+            }
+
+            await _repositorio.NovoVeiculoAsync(veiculo, fileName);
+
+            return Created($"api/Veiculos", veiculo);
         }
     }
 }
--- Backend/CarZone/src/Models/Veiculo.cs
+++ Backend/CarZone/src/Models/Veiculo.cs
@@ -8,6 +8,6 @@
         public int Ano { get; set; }
         public decimal Valor { get; set; }
         public string Tipo { get; set; }
-        public string ImagemUrl { get; set; }
+        public string? ImagemUrl { get; set; }
     }
 }
